a14851ce6a3cc4acc706de3d148713393a3bba37
miralyaser9 <https://github.com/miralyaser9>
2024-03-09 01:14:31 +0200

    firstt

---
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Splash_view extends StatelessWidget {
+  const Splash_view({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
